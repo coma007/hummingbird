edcef9018ec628990d0d9a254ff83fe347cb7e07
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-05-17 15:42:38 +0200

    feat: Loading dataset

---
+++ load_dataset.py
@@ -0,0 +1,50 @@
+#package for music and audio analysis
+import librosa
+import numpy as np
+import os
+# for progress visualization
+from tqdm import tqdm
+# for csv files
+import pandas as pd
+
+def mfccs_feature_extractor(file):
+    
+    audio, sample_rate = librosa.load(file, res_type='kaiser_fast') 
+    
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc = 40)
+    
+    #Scaled mfccs to its mean
+    mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    
+    return mfccs_scaled_features
+
+def load_dataset(path):
+    # print(path)
+    # abs_path = os.path.abspath(path)
+    # print(abs_path)
+
+    #labels and feature
+    data = []
+
+    for folder in tqdm(os.listdir(path)):
+        if ".csv" in folder:
+            if "Audio" not in folder:
+                continue
+            new_path = path + '\\' + folder
+            load_metadata(new_path, data)
+            continue
+        if "Frozen" not in folder:
+            continue
+        for file in os.listdir(path+"/"+folder):
+            features = mfccs_feature_extractor(path+"\\"+folder+"\\"+file)
+            index = int(file[:-4])
+            data[index].append(features)
+    return data
+
+def load_metadata(path, data):
+    df = pd.read_csv(path)
+    for i in range(len(df[list(df.keys())[0]])):
+        data.append([])
+        for column in df:
+            data[i].append(df[column][i])
+    # print(data[0])
