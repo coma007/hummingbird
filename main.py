c141f2690275503e9c9fa898dc1c7d3dedb9a49b
coma007 <milicasladakovic7@gmail.com>
2023-05-17 16:37:48 +0200

    Join path fixed

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from load_dataset import load_dataset
 import pandas as pd
 
+
 def main():
-    data = load_dataset("dataset")
-    mlmp_df = pd.DataFrame(data,columns=['song_file', 'interpreter', 'label','audio_type','mfccs'])
+    data = load_dataset("small_dataset")
+    mlmp_df = pd.DataFrame(
+        data, columns=['song_file', 'interpreter', 'label', 'audio_type', 'mfccs'])
     print(mlmp_df)
+
 
 if __name__ == "__main__":
     main()
